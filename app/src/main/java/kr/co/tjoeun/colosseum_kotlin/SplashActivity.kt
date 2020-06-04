7008071224bc6a31d3fd9524ebbe6fb886e50a07
yhwba <59465345+yhwba@users.noreply.github.com>
2020-06-04 11:55:05 +0900

    GlobaData  클래스를 이용해서, 로그인 한 사용자 정보를 공유

--- app/src/main/java/kr/co/tjoeun/colosseum_kotlin/SplashActivity.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_kotlin/SplashActivity.kt
@@ -3,8 +3,13 @@ package kr.co.tjoeun.colosseum_kotlin
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_splash.*
+import kr.co.tjoeun.colosseum_kotlin.datas.GlobalData
+import kr.co.tjoeun.colosseum_kotlin.datas.User
 import kr.co.tjoeun.colosseum_kotlin.utils.ContextUtil
+import kr.co.tjoeun.colosseum_kotlin.utils.ServerUtil
+import org.json.JSONObject
 
 class SplashActivity : BaseActivity() {
 
@@ -22,9 +27,11 @@ class SplashActivity : BaseActivity() {
 
     override fun setValues() {
 
+        getMyInfoFromServer()
+
         Handler().postDelayed({
 
-            if(ContextUtil.isAutoLogin(mContext) && ContextUtil.getLoginUserToken(mContext) !=""){
+            if(GlobalData.loginUser != null){
                 val myIntent = Intent(mContext, MainActivity::class.java)
                 startActivity(myIntent)
             }
@@ -35,6 +42,29 @@ class SplashActivity : BaseActivity() {
             finish()
 
         }, 2000)
+
+    }
+//    내 정보 데이터를 서버에 요청하는 데이터
+    fun getMyInfoFromServer(){
+//        자동로그인이 된 경우
+        if (ContextUtil.isAutoLogin(mContext) && ContextUtil.getLoginUserToken(mContext) !=""){
+            ServerUtil.getRequestUserInfo(mContext, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(json: JSONObject?) {
+                    Log.d("내 정보",json.toString())
+
+                    val data = json!!.getJSONObject("data")
+                    val user = data.getJSONObject("user")
+
+                    val loginUser = User.getUserFromJson(user)
+
+                    GlobalData.loginUser = loginUser
+
+
+                }
+
+
+            })
+        }
 
     }
 
---
+++ app/src/main/java/kr/co/tjoeun/colosseum_kotlin/datas/GlobalData.java
@@ -0,0 +1,8 @@
+package kr.co.tjoeun.colosseum_kotlin.datas;
+
+public class GlobalData {
+
+//    로그인 한 사용자 정보를 공유시켜주는 객체
+//    앱을 처음 킨 순간에는 아직 로그인 한 사람이 없는 걸로 간주하자
+    public static  User loginUser = null;
+}
