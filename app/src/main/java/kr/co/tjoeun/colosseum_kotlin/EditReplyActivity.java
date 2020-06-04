8f38fc5ddf0fa14bf35df6f94f004ec79b769d6e
yhwba <59465345+yhwba@users.noreply.github.com>
2020-06-04 12:19:52 +0900

    의견 수정하기 완료

--- app/src/main/java/kr/co/tjoeun/colosseum_kotlin/EditReplyActivity.java
+++ app/src/main/java/kr/co/tjoeun/colosseum_kotlin/EditReplyActivity.java
@@ -1,8 +1,11 @@
 package kr.co.tjoeun.colosseum_kotlin;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -19,16 +22,16 @@ public class EditReplyActivity extends BaseActivity {
     String mySideTitle;
     int topicId;
 
-//    의견 수정시에는 값이 -1 이 아니게 변경할 예정
+    //    의견 수정시에는 값이 -1 이 아니게 변경할 예정
 //     -1이면 새로 등록하는 경우.
-    int replyId =-1;
+    int replyId = -1;
 
     ActivityEditReplyBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding= DataBindingUtil.setContentView(this,R.layout.activity_edit_reply);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_reply);
         setupEvents();
         setValues();
     }
@@ -39,18 +42,18 @@ public class EditReplyActivity extends BaseActivity {
         binding.postBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String input = binding.contentEdt.getText().toString();
+                final String input = binding.contentEdt.getText().toString();
 
-                if(replyId ==-1){
+                if (replyId == -1) {
                     ServerUtil.postRequestReply(mContext, topicId, input, new ServerUtil.JsonResponseHandler() {
                         @Override
                         public void onResponse(JSONObject json) {
-                            Log.d("댓글달기 응답",json.toString());
+                            Log.d("댓글달기 응답", json.toString());
 
                             try {
                                 int code = json.getInt("code");
 
-                                if (code == 200){
+                                if (code == 200) {
                                     runOnUiThread(new Runnable() {
                                         @Override
                                         public void run() {
@@ -58,9 +61,8 @@ public class EditReplyActivity extends BaseActivity {
                                             finish();
                                         }
                                     });
-                                }
-                                else{
-                                    final String message= json.getString("message");
+                                } else {
+                                    final String message = json.getString("message");
                                     runOnUiThread(new Runnable() {
                                         @Override
                                         public void run() {
@@ -74,17 +76,36 @@ public class EditReplyActivity extends BaseActivity {
                             }
                         }
                     });
-                }
-                else {
+                } else {
 //                    댓글을 수정해야하는 경우
 
-                    ServerUtil.putRequestReply(mContext, replyId, input, new ServerUtil.JsonResponseHandler() {
+                    AlertDialog.Builder alert = new AlertDialog.Builder(mContext);
+                    alert.setTitle("의견 수정");
+                    alert.setMessage("정말 의견을 수정 하시겠습니까? 의견을 수정해도 이전 내용은 계속 보관됩니다.");
+                    alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
                         @Override
-                        public void onResponse(JSONObject json) {
-                            Log.d("수정",json.toString());
+                        public void onClick(DialogInterface dialog, int which) {
+                            ServerUtil.putRequestReply(mContext, replyId, input, new ServerUtil.JsonResponseHandler() {
+                                @Override
+                                public void onResponse(JSONObject json) {
+                                    Log.d("수정", json.toString());
 
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+//                                            코드가 200인걸 확인하고 의견 수정 완료된거를 찍는것이 더 좋음.
+                                            Toast.makeText(mContext, "의견 수정이 완료되었습니다.", Toast.LENGTH_SHORT).show();
+                                            finish();
+                                        }
+                                    });
+
+                                }
+                            });
                         }
                     });
+                    alert.setNegativeButton("취소", null);
+                    alert.show();
+
 
                 }
 
@@ -101,18 +122,18 @@ public class EditReplyActivity extends BaseActivity {
         mySideTitle = getIntent().getStringExtra("sideTitle");
         binding.sideTitleTxt.setText(mySideTitle);
 
-        topicId =getIntent().getIntExtra("topicId",-1);
+        topicId = getIntent().getIntExtra("topicId", -1);
 
 //        어느 댓글을 수정할지 id를 받아서 replyId 변수에 저장
         replyId = getIntent().getIntExtra("replyId", -1);
 
-        if( replyId !=-1){
+        if (replyId != -1) {
             setTitle("의견 수정하기");
             binding.postBtn.setText("의견 수정하기");
 
             binding.contentEdt.setText(getIntent().getStringExtra("content"));
-        }
-        else {
+
+        } else {
             setTitle("의견 등록하기");
         }
 
